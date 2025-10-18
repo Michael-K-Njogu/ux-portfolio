f7960f57ba6d4a8a2e3e275ac56d762a24d760f0
Michael-K-Njogu <mikekjnogu@gmail.com>
2025-10-18 14:58:07 +0300

    Imported fonts

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css'; // Import Bootstrap CSS
+import '@fontsource-variable/plus-jakarta-sans'; // Import Plus Jakarta Sans font
+import '@fontsource-variable/instrument-sans'; // Import Instrument Sans font
+import './styles/main.css'; // Import the main CSS file
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(

46abed2153814d119598570b7245e732112f4c49
Michael-K-Njogu <mikekjnogu@gmail.com>
2025-10-18 14:57:51 +0300

    Added case studies component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-import CaseStudies from './components/caseStudies';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './components/layout/Header';
+import Footer from './components/layout/Footer';
+import CaseStudies from './components/CaseStudies';
+import CaseStudyDetail from './components/CaseStudyDetail';
 
 function App() {
   return (
-    <div className="App">
-      <CaseStudies />
-    </div>
+    <Router>
+      <div className="d-flex flex-column min-vh-100">
+      <Header />
+
+      <main>
+      <Routes>
+        <Route path="/" element={<CaseStudies />} />
+        <Route path="/case-study/:slug" element={<CaseStudyDetail />} />
+      </Routes>
+      </main>
+
+      <Footer />
+      </div>
+    </Router>
   );
 }
 

f118cd69c05e5af5e093cd0555e6a7f41fc9d3e1
Michael-K-Njogu <mikekjnogu@gmail.com>
2025-11-02 19:22:01 +0300

    Updated imports

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Header from './components/layout/Header';
 import Footer from './components/layout/Footer';
 import About from './pages/About'
 import NotFound from './pages/NotFound'
-import CaseStudies from './components/caseStudies';
+import CaseStudies from './components/AllCaseStudies';
 import CaseStudyDetail from './components/CaseStudyDetail';
 
 const App = () => {

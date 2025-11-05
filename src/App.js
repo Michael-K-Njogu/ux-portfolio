c8e4c4067decb2352dac3841ebcae1c950495911
Michael-Njogu <mykekunyo@gmail.com>
2025-11-05 15:05:26 +0300

    Simplify App.js layout by using Outlet for nested routes under RouterProvider

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
-import { Routes, Route } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 import Header from './components/layout/Header';
 import Footer from './components/layout/Footer';
-import About from './pages/About'
-import NotFound from './pages/NotFound'
-import CaseStudies from './components/AllCaseStudies';
-import CaseStudyDetail from './components/CaseStudyDetail';
 import BackToTop from './components/ui/BackToTop';
 
 const App = () => {
   return (
-    <div>
+    <>
       <Header />
-      <Routes>
-        <Route index element={<CaseStudies />} />
-        <Route path="case-studies/:slug" element={<CaseStudyDetail />} />
-        <Route path="about" element={<About />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
+      <Outlet /> {/* ← this is where nested routes will render */}
       <BackToTop />
       <Footer />
-    </div>
+    </>
   );
-}
+};
 
 export default App;
